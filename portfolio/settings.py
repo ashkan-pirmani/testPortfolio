b18f73d64a29a8565193d248b115c26193ea4bc1
Ashkan Pirmani <55592879+ashkan-pirmani@users.noreply.github.com>
2022-10-04 10:03:49 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r#y2p)#22&_!yjcmw$&dj7n=x6ty0!r6@ieei218=n-iqspzmf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
